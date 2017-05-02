c61f6388af899ccb5b16c6d5aa6efd648f699846
exclusive <exclusive@zivitech.com>
2017-05-02 12:55:27 +0300

    uploaderSSS

--- lib/carrierwave/sequel.rb
+++ lib/carrierwave/sequel.rb
@@ -54,6 +54,49 @@ module CarrierWave
       include CarrierWave::Sequel::Hooks
     end
 
+    def mount_uploaders(column, uploader, options={}, &block)
+      raise "You need to use Sequel 3.0 or higher. Please upgrade." unless ::Sequel::Model.respond_to?(:plugin)
+      super
+
+      mod = Module.new
+      include mod
+
+      mod.class_eval <<-RUBY, __FILE__, __LINE__+1
+        def #{column}=(new_files)
+          if !(new_files.blank? && send(:#{column}).blank?)
+            modified!
+          end
+          super
+        end
+
+        def remove_#{column}!
+          modified!
+          super
+        end
+
+        def remove_#{column}=(value)
+          modified!
+          super
+        end
+
+        # Reset cached mounter on record reload
+        def reload(*)
+          @_mounters = nil
+          super
+        end
+
+        def remote_#{column}_urls=(urls)
+          modified!
+          super
+        end
+      RUBY
+
+      alias_method :read_uploader, :[]
+      alias_method :write_uploader, :[]=
+
+      include CarrierWave::Sequel::Hooks
+    end
+
   end # Sequel
 end # CarrierWave
 
